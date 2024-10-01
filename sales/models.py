c52284a4a6e3d894b62d7ec2be7defe2867fe68f
SJagenow <sjagenow@web.de>
2024-10-01 10:29:16 +0200

    db update

--- sales/models.py
+++ sales/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    newsletter_abo = models.BooleanField()
-    email_adress = models.EmailField()
-    account = models.FloatField()
+    newsletter_abo = models.BooleanField(default=True)
+    email_adress = models.CharField(max_length=30, blank=True, default="")
+    account = models.FloatField(blank=True, null=True)
+
