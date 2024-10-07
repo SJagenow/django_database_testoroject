9ccb65a32898f70ae7e511c89fefccea4a5344c9
SJagenow <sjagenow@web.de>
2024-10-07 10:43:27 +0200

    one to many  many to many

--- sales/models.py
+++ sales/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
 # Create your models here.
-
+#many
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=True)
     email_adress = models.CharField(max_length=30, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
-
-class Order(models.Model):
-    #many-to-one Customer
-    #one-to-one Bill
-    pass
-
+#one
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
     #many-to-many Order
+    
+class Order(models.Model):
+    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+class Producttype(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.Cascade)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    type_name = models.CharField(max_length=300)
+    
 
     class Bill(models.Model):
      total_amount = models.FloatField()
