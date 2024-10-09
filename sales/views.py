92ae74a162480e1cf5bbcbfb71922c1248557b26
SJagenow <sjagenow@web.de>
2024-10-09 14:22:59 +0200

    full test

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,30 @@
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Customer
+from django.views.generic.detail import DetailView
+from django.utils import timezone
 # Create your views here.
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = "sales/list.html"
+    paginate_by = 3
+
+class CustomerListSearchView(CustomerListView):
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        return Customer.objects.filter(first_name=name)
+    
+
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = "sales/detail.html"
+
+    def get_object(self):
+        obj = super().get_object()
+        obj.last_accessed = timezone.now()
+        return obj
